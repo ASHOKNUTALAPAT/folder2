6b3e31d49371d5fdc528d84a857e520bbcc8acc7
nutalapati.ashok <nutalapati.ashok@gmail.com>
2023-07-21 15:40:11 +0530

    version1.0

---
+++ jspdemo/src/main/java/jspdemo/StudentBean.java
@@ -0,0 +1,22 @@
+package jspdemo;
+
+public class StudentBean {
+	private int rollno;
+	private String name;
+	public StudentBean() {
+		super();
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
